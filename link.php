128b6ad6c247d908a755e69318b201ae2489cdfc
Kevin Bastian <kjb5642@rit.edu>
2017-06-20 19:40:42 -0500

    Deleting duplicate files

--- link.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*$link=mysql_connect($_SERVER["serverdata"],"samtec","sam33");
-mysql_set_charset('utf8',$link);*/
-$link = new mysqli($_SERVER["serverdata"],"samtec","sam33");
-$link->set_charset("utf8");
-?>
