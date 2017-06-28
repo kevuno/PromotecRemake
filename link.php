eb12f94001a67f7f7175084d0c2056056a2ba22a
Kevin Bastian <kjb5642@rit.edu>
2017-06-27 19:31:04 -0500

    Changed link data

--- link.php
+++ link.php
@@ -6,7 +6,8 @@ class link{
 	static function getLink(){
 		try{
 			$ip = $_SERVER["serverdata"];
-			$link = new mysqli($ip,"samtec","sam33");	
+			//$link = new mysqli($ip,"samtec","sam33");	
+			$link = new mysqli("localhost","root","Bacardi12312300");	
 		}catch(Exception $e){
 			throw new Exception("Error intentando contectarse a la Base de datos");
 		}
